f95e2a9b51f17cfe0cd7c64e3cd777309ee8acae
Madara <kushalbhandari9998@gmail.com>
2026-07-28 08:12:51 +0545

    Update Question10.cs

--- CSharpTutorial/Question10.cs
+++ CSharpTutorial/Question10.cs
@@ -1,2 +1,18 @@
-using system;
+using System;
 
+class Question10
+{
+    static void main()
+    {   
+        
+double celsius = 25.6;
+
+  double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+Console.WriteLine($"the {celsius} is equal to {fahrenheit} ");
+
+
+
+    }
+
+}
